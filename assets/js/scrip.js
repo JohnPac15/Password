804442989ac76783355be7882af0b30348f6e453
JohnPac <johnpac15@yahoo.com>
2021-11-17 15:43:56 -0600

    adding a test section

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -1,14 +1,4 @@
-// var YO = "sup";
-// var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" ];
-// var symbols =[ "!,@,#,$,%,^,&,*,?" ];
-// var numbers = [ "1,2,3,4,5,6,7,8,9,0" ];
-// var passwordLength = 
 
-// var passwordspecial = function(){
-//     for (var i=0; i < lettersadd.length; i++) {
-//         console.log(lettersadd[i])
-//     }
-// };
 const finalpassword = document.getElementById("password");
 
 
@@ -16,7 +6,7 @@ document.getElementById("button").addEventListener("click", function password ()
     
 
     var lower = upper = number = special = false
-    var passwordLength = window.prompt(" How long would you like your pass to be? Chose between 8 & 128 charaters ")
+    var passwordLength = window.prompt(" How long would you like your password to be? Chose between 8 & 128 charaters ")
     passwordLength = parseInt(passwordLength,10);
     console.log(passwordLength)
     
@@ -26,10 +16,10 @@ document.getElementById("button").addEventListener("click", function password ()
             console.log(" this is a number ")
             if( passwordLength >= 8 && passwordLength <= 128) {
                 console.log(" this works ")
-                lower = window.confirm( " use lower case values? ")
-                upper = window.confirm( " use upper case values? ")
-                number = window.confirm( " use numbers in password ")
-                special = window.confirm( " use special case in password ")
+                lower = window.confirm( " Do you want to include lower case letters? ")
+                upper = window.confirm( " Do you want to use upper case letters? ")
+                number = window.confirm( " Do you want to use numbers in your password ")
+                special = window.confirm( " do you want to use special charaters in password ")
                 console.log({lower, upper, number, special})
 
                 let temp = String()
